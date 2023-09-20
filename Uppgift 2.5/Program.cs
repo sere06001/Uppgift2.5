2f721758db03895fa5017123a9ee028a6bc88b4c
sere06001 <sere06001@utb.vaxjo.se>
2023-09-20 10:47:45 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift2_5
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Skriv en multiplikation.");
+            string hela = Console.ReadLine();
+            int multTecken = hela.IndexOf("*");
+
+            double tal1 = double.Parse(hela[..multTecken]);
+            double tal2 = double.Parse(hela[(multTecken + 1)..]);
+
+            double summa = tal1 * tal2;
+
+            Console.WriteLine(tal1 + " * " + tal2 + " = " + summa);
+            Console.ReadKey();
+        }
+    }
+}
